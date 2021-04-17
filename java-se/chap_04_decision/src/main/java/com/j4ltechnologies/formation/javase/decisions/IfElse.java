c69608a9574657e89a9e32bee4ad9f36fcb0e39c
Joachim Zadi <joachim.zadi@j4ltechnologies.com>
2021-04-17 21:18:13 +0200

    chap_03 chap_04

---
+++ java-se/chap_04_decision/src/main/java/com/j4ltechnologies/formation/javase/decisions/IfElse.java
@@ -0,0 +1,31 @@
+package com.j4ltechnologies.formation.javase.decisions;
+
+import java.util.Scanner;
+
+/**
+ * Ce programme simule un panneau d'ascenseur qui saute le 13e étage.
+ */
+public class IfElse {
+    public static void main(String[] args) {
+        Scanner saisie = new Scanner(System.in);
+        System.out.print("Etage: ");
+
+        if (saisie.hasNextInt()) {
+            //Ici l" user a saisi un entier
+            int etage = saisie.nextInt();
+
+            // Ajuster le sol si nécessaire
+            int etageActuel;
+
+            if (etage > 13) {
+                etageActuel = etage - 1;
+            } else {
+                etageActuel = etage;
+            }
+            System.out.println("L'ascenseur se rendra à l'étage " + etageActuel);
+        } else {
+            System.out.println("Attention à votre saisie, un entier est attendu en entrée !!!");
+        }
+        saisie.close();
+    }
+}
