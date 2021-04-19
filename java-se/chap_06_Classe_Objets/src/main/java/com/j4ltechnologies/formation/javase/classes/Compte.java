c81b66a763fc8879ac8979d617f26e49b7e0343f
Joachim Zadi <joachim.zadi@j4ltechnologies.com>
2021-04-19 20:11:35 +0200

    chap_06

---
+++ java-se/chap_06_Classe_Objets/src/main/java/com/j4ltechnologies/formation/javase/classes/Compte.java
@@ -0,0 +1,64 @@
+package com.j4ltechnologies.formation.javase.classes;
+
+/**
+ * Classe Compte, créée le 19/04/2021 à 19:52
+ * Declarer une classe, c'est declarer un nouveau type en Java
+ *
+ * @author Joachim Zadi
+ * @version 1.0 du 19/04/2021
+ */
+public class Compte {
+    // Les attributs ==> de preference private
+    // L'encapsulation ==> private
+    private String numero;
+    private double solde;
+
+    //Attricbut de classe
+    private static int nbCompte;
+
+    // Un constructeur de la classe
+    public Compte(String n) {
+        numero = n;
+        solde = 80;
+        nbCompte++;
+    }
+
+    // Surcharge du constructeur
+    public Compte(String n, double montant) {
+        numero = n;
+        solde = montant + 80;
+        nbCompte++;
+    }
+
+    //Methode de classe
+    public static int getNbCompte() {
+        return nbCompte;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void crediter(double montant) {
+        if (montant > 0) {
+            solde += montant;
+        }
+    }
+
+    public boolean debiter(double montant) {
+        if (montant > solde) {
+            return false;
+        } else {
+            solde -= montant;
+            return true;
+        }
+    }
+
+    public String description() {
+        return String.format("Compte(N°: %s, Solde : %s)", numero, solde);
+    }
+}
