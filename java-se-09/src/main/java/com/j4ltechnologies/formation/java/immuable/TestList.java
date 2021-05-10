c27267e644bd50cd7066ab155d1b4b72c4e1287f
Joachim Zadi <joachim.zadi@j4ltechnologies.com>
2021-05-10 16:18:36 +0200

    java-se-09 Stream

---
+++ java-se-09/src/main/java/com/j4ltechnologies/formation/java/immuable/TestList.java
@@ -0,0 +1,23 @@
+package com.j4ltechnologies.formation.java.immuable;
+
+import java.util.List;
+
+/**
+ * Classe TestList, créée le 10/05/2021 à 15:56
+ *
+ * @author Joachim Zadi
+ * @version 1.0 du 12/04/2021
+ */
+public class TestList {
+    public static void main(String[] args) {
+        // On definit une liste immuable
+        List<String> prenoms = List.of("Joachim", "Eric", "Jean");
+
+        // On conserve l'ordre des elements
+        System.out.println(prenoms);
+
+        prenoms.add("Power");// Leve une exception UnsupportedOperationException car la liste est immuable
+
+        prenoms = List.of("Joachim", "Eric", "Jean", null);// Ne compile car les elements doivent etre non nuls
+    }
+}
---
+++ java-se-09/src/main/java/com/j4ltechnologies/formation/java/streams/TestStreamJava9.java
@@ -0,0 +1,16 @@
+package com.j4ltechnologies.formation.java.streams;
+
+/**
+ * Classe TestStreamJava9, créée le 10/05/2021 à 15:50
+ *
+ * @author Joachim Zadi
+ * @version 1.0 du 12/04/2021
+ */
+public class TestStreamJava9 {
+    public static void main(String[] args) {
+        // Les nouvelles méthodes takeWhile et dropWhile vous permettent
+        // d'obtenir des parties d'un flux en fonction d'un prédicat.
+        // Ici, un flux peut être commandé ou non, donc:
+
+    }
+}
